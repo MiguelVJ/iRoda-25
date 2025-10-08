410015e713586d99de9ee0ad05df479210d87bf5
MiguelVJ <miguel.vaz.jim@hotmail.com>
2025-10-08 11:34:39 -0600

    Cambios de nombre Lua

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# iRodaCA
-# iRoda-25
+# LUA Studio 2025
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'iRoda',
+  appName: 'LUA Studio',
   webDir: 'www',
   server: {
     androidScheme: 'https'
